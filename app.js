76f8dc30f155ecd44e6e9c397e1c00235a4be3f6
Hao_cen <haocen.minecraft@gmail.com>
2021-10-19 06:20:17 +0000

    修复cache.json不会自动生成的问题 添加自适应渠道 现在将会自动用cookie获取绑定的游戏UID和region

--- app.js
+++ app.js
@@ -1,73 +1,87 @@
-const request = require('request');
+// const request = require('request');
 const express = require('express');
 const app = express();
 const rp = require('request-promise');
 const fs = require('fs');
 const moment = require('moment');
-const cache = require('./cache.json');
-const port = 12583;
-const config = require('./config.json')
-const uid = config['uid']
-const apiService = `https://api-takumi.mihoyo.com/game_record/app/genshin/api/dailyNote?role_id=${uid}&server=cn_gf01`;
+const util = require('util');
+// const cache = require('./cache.json');
+const config = require('./config.json');
+const dailyNoteUrl = `https://api-takumi.mihoyo.com/game_record/app/genshin/api/dailyNote?role_id=%s&server=%s`;
 
+var cache;
 require('./lib/global').init();
 
-app.get('/resin', function (req, res) {
-	console.log('[%s][Get] /resin ',moment().utcOffset('+08:00').format());
-	if (Math.floor(Date.now() / 1000) - cache['last_update'] > 300) {
-		console.log('Updating Data ...');
-		rp({
-			headers: getHeader(apiService),
-			resolveWithFullResponse: true,
-			method: 'GET',
-			uri: apiService,
-		}).then(function (response) {
-			if (response.statusCode != 200) {
-				console.log('failed.')
-				res.status(404);
-			} else {
-				let feedback = JSON.parse(response.body).data;
-				cache['current_resin'] = feedback['current_resin'];
-				cache['max_resin'] = feedback['max_resin'];
-				cache['resin_recovery_time'] = feedback['resin_recovery_time'];
-				cache['finished_task_num'] = feedback['finished_task_num'];
-				cache['total_task_num'] = feedback['total_task_num'];
-				cache['is_extra_task_reward_received'] = feedback['is_extra_task_reward_received'],
-				cache['max_expedition_num'] = feedback['max_expedition_num'];
-				var finished_expedition_num = 0;
-				feedback.expeditions.forEach(element => {
-					if (element.status == 'Finished') {
-						finished_expedition_num = finished_expedition_num + 1;
-					}
-				});
-				cache['finished_expedition_num'] = finished_expedition_num;
-				cache['last_update'] = Math.floor(Date.now() / 1000);
-				cache['last_update_format'] = moment().utcOffset('+08:00').format();
-				
-				var cacheStr = JSON.stringify(cache);
-				fs.writeFile('./cache.json', cacheStr, function (err) {
-					if (err) {
-						console.log(err);
-					} else {
-						console.log('Resin cache update -',Math.floor(Date.now() / 1000));
-						genResult(res);
-					}
-				});
-		
-			}
-		}).catch(function (error) {
-			console.log(error);
-			res.status(404);
-		});
-	} else {
-		console.log('Sending Data from cache...');
-		genResult(res);
-	}
-});
+const init = async function() {
 
-app.listen(port, '0.0.0.0', function () {
-	console.log('Application is listening on port ' + port + '!')
-})
+	if(!fs.existsSync('./cache.json')) {
+		await fs.writeFileSync('./cache.json',"{}");
+	}
+	cache = require('./cache.json');
+
+	if (cache['region'] == undefined || cache['game_uid'] == undefined) {
+		console.log("玩家数据不存在，正在尝试获取...");
+		await refPlayer();
+	}
+	app.get('/resin', function (req, res) {
+		console.log('[%s][Get] /resin ',moment().utcOffset('+08:00').format());
+		if (cache['last_update'] == undefined || Math.floor(Date.now() / 1000) - cache['last_update'] > config.cache_time) {
+			console.log('Updating Data ...');
+			var apiService = util.format(dailyNoteUrl,cache['game_uid'],cache['region']);
+			rp({
+				headers: getHeader(apiService),
+				resolveWithFullResponse: true,
+				method: 'GET',
+				uri: apiService,
+			}).then(function (response) {
+				if (response.statusCode != 200) {
+					console.log('failed.')
+					res.status(404);
+				} else {
+					let feedback = JSON.parse(response.body).data;
+					cache['current_resin'] = feedback['current_resin'];
+					cache['max_resin'] = feedback['max_resin'];
+					cache['resin_recovery_time'] = feedback['resin_recovery_time'];
+					cache['finished_task_num'] = feedback['finished_task_num'];
+					cache['total_task_num'] = feedback['total_task_num'];
+					cache['is_extra_task_reward_received'] = feedback['is_extra_task_reward_received'],
+					cache['max_expedition_num'] = feedback['max_expedition_num'];
+					var finished_expedition_num = 0;
+					feedback.expeditions.forEach(element => {
+						if (element.status == 'Finished') {
+							finished_expedition_num = finished_expedition_num + 1;
+						}
+					});
+					cache['finished_expedition_num'] = finished_expedition_num;
+					cache['last_update'] = Math.floor(Date.now() / 1000);
+					cache['last_update_format'] = moment().utcOffset('+08:00').format();
+					
+					var cacheStr = JSON.stringify(cache);
+					fs.writeFile('./cache.json', cacheStr, function (err) {
+						if (err) {
+							console.log(err);
+						} else {
+							console.log('Resin cache update -',Math.floor(Date.now() / 1000));
+							genResult(res);
+						}
+					});
+			
+				}
+			}).catch(function (error) {
+				console.log(error);
+				res.status(404);
+			});
+		} else {
+			console.log('Sending Data from cache...');
+			genResult(res);
+		}
+	});
+
+	app.listen(config.port, '0.0.0.0', function () {
+		console.log('Application is listening on port ' + config.port + '!')
+	});
+}
+init();
 
 function genResult(res) {
 	var result = {};
@@ -86,4 +100,27 @@ function genResult(res) {
 	result['last_update'] = cache['last_update_format'];
 	res.status(200);
 	res.send(JSON.stringify(result));
+}
+
+const refPlayer = async function() {
+	await rp({
+		headers: getHeaderOld(),
+		resolveWithFullResponse: true,
+		method: 'GET',
+		uri:"https://api-takumi.mihoyo.com/binding/api/getUserGameRolesByStoken"
+	}).then(function(response){
+		// console.log(response.body);
+		var pList = JSON.parse(response.body);
+		pList.data.list.forEach(playerData => {
+			// console.log('GetDeafultRegion [%s] %s',playerData.game_biz,playerData);
+			var cache = {};
+			if (playerData.game_biz == config.game_biz && playerData.is_chosen) {
+				cache['last_update'] = 0;
+				cache['region'] = playerData.region;
+				cache['game_uid'] = playerData.game_uid;
+				fs.writeFileSync('./cache.json',JSON.stringify(cache));
+				console.log('Refresh player info [%s - %s]',cache['region'],cache['game_uid']);	
+			}
+		  });
+	});
 }
